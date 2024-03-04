933e58902453cee9160dfd451bd567a1e495f748
Occideree <incorporeall@yandex.com>
2024-03-04 15:45:40 +0500

    1.7. Начинаем программировать

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/main.js",
+  output: {
+    filename: "bundle.[contenthash].js",
+    path: path.resolve(__dirname, "build"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  devtool: "source-map",
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [{ from: "public", to: "build" }],
+      globOptions: {
+        ignore: ["**/index.html"],
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
